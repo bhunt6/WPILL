6169281e994de3d6c9eeeca113159df9fd3c76e2
Ben Hunt <benjaminhunt93@gmail.com>
2022-02-14 15:36:53 -0500

    update filepaths; add current issue

--- issues.js
+++ issues.js
@@ -1,6 +1,37 @@
 issues = [
     {
-        "id": "Volume 2, Spring 1993",
+        "id": "Volume 1 - Spring 1992",
+        "editor": ["Thomas Purnell"],
+        "article": [
+            {
+                "author": "Jun Aramaki:",
+                "title": "From a Myth to an Article of Faith",
+                "file": "texts/issue_1/1_Aramaki.pdf"
+            },
+            {
+                "author": "P. Hironymous:",
+                "title": "Third Person Indicative /-s/: Constraints on Second Language Acquisition",
+                "file": "texts/issue_1/1_Hironymous.pdf"
+            },
+            {
+                "author": "Gigi McGuire Leach:",
+                "title": "American Sign Language and the Second Language Learner: the Influence of Modality on Adult Second Language Acquisition",
+                "file": "texts/issue_1/1_Leach.pdf"
+            },
+            {
+                "author": "Melisa Lozano:",
+                "title": "Some Notes on Stalnaker's Possible-World Theory of Propositions",
+                "file": "texts/issue_1/1_Lozano.pdf"
+            },
+            {
+                "author": "Thomas Purnell:",
+                "title": "Practical Theory: Effects of Postlexical Rules on Second Language Pronunciation.",
+                "file": "texts/issue_1/1_Purnell.pdf"
+            }
+        ]
+    },
+    {
+        "id": "Volume 2 - Spring 1993",
         "editor": [
             "William Fleitz",
             "Takako Kawasaki",
@@ -10,32 +41,32 @@ issues = [
             {
                 "author": "Thomas Purnell:",
                 "title": "A Clarification of Asian Tonal Features",
-                "file": "2_Purnell.pdf"
+                "file": "texts/issue_2/2_Purnell.pdf"
             },
             {
                 "author": "Carl Bailey:",
                 "title": "\"He's dumb as hell, but he ain't crazy\": A Psycholinguistic Analysis of Steinbeck's Lennie Small",
-                "file": "2_Bailey.pdf"
+                "file": "texts/issue_2/2_Bailey.pdf"
             },
             {
                 "author": "Mary Meares:",
                 "title": "Parallel Distributed Processing and Universal Grammar: Complementary Concepts in a Biological Experiment for the Critical Period in Language Acquisition",
-                "file": "2_Meares.pdf"
+                "file": "texts/issue_2/2_Meares.pdf"
             },
             {
                 "author": "Takako Kawasaki:",
                 "title": "Parameter Settings and the Subset Principle in L2 Acquisition of the Binding Theory",
-                "file": "2_Kawasaki.pdf"
+                "file": "texts/issue_2/2_Kawasaki.pdf"
             },
             {
                 "author": "Stephen Hayle:",
                 "title": "On the Null Subjects of Chinese Weather Verbs",
-                "file": "2_Hayle.pdf"
+                "file": "texts/issue_2/2_Hayle.pdf"
             }
         ]
     },
     {
-        "id": "Volume 3, Spring 1996",
+        "id": "Volume 3 - Spring 1996",
         "editor": [
             "Shelby Largay",
             "Terry Stryer"
@@ -44,62 +75,62 @@ issues = [
             {
                 "author": "Charles Baker:",
                 "title": "Vowel Epenthesis by Spanish Learners of English",
-                "file": "3_Baker.pdf"
+                "file": "texts/issue_3/3_Baker.pdf"
             },
             {
                 "author": "Cam Esser:",
                 "title": "How Does Motivation to Learn a Second Language Differ from Other Performance Skill Motivation?",
-                "file": "3_Esser.pdf"
+                "file": "texts/issue_3/3_Esser.pdf"
             },
             {
                 "author": "Sarah Roman:",
                 "title": "Two Models for Understanding Differential Substitution in Interlanguage",
-                "file": "3_Roman.pdf"
+                "file": "texts/issue_3/3_Roman.pdf"
             },
             {
                 "author": "Penelope Roberts:",
                 "title": "Maximally Unspecified Vowels in Farsi",
-                "file": "3_Roberts.pdf"
+                "file": "texts/issue_3/3_Roberts.pdf"
             },
             {
                 "author": "Alexanders Starz:",
                 "title": "On Modularity",
-                "file": "3_Starz.pdf"
+                "file": "texts/issue_3/3_Starz.pdf"
             }
         ]
     },
     {
-        "id": "Volume 4, Spring 1997",
-        "editor": "Stephanie Buchner",
+        "id": "Volume 4 - Spring 1997",
+        "editor": ["Stephanie Buchner"],
         "article": [
             {
                 "title": "acknowledgements",
-                "file": "4_ack.pdf"
+                "file": "texts/issue_4/4_ack.pdf"
             },
             {
                 "author": "Marisa Schaer:",
                 "title": "The Occurrence of Gerunds in CP Clauses",
-                "file": "4_Schaer.pdf"
+                "file": "texts/issue_4/4_Schaer.pdf"
             },
             {
                 "author": "Carol Brouha:",
                 "title": "Variance in Thematic Roles Projected by Middles: A Case for Formal Role Subcategorization Grids",
-                "file": "4_Brouha.pdf"
+                "file": "texts/issue_4/4_Brouha.pdf"
             },
             {
                 "author": "Namassawan Padibat and Caroline Cochran:",
                 "title": "An Investigation of Sonority Dispersion in Thai-English Interlanguage Codas",
-                "file": "4_Patibat.pdf"
+                "file": "texts/issue_4/4_Patibat.pdf"
             },
             {
                 "author": "Stephanie Buchner:",
                 "title": "Consonant Harmony: A Comparison of Two Explanations Based On Underspecification Theory.",
-                "file": "4_Buchner.pdf"
+                "file": "texts/issue_4/4_Buchner.pdf"
             }
         ]
     },
     {
-        "id": "Volume 5, Spring 1998",
+        "id": "Volume 5 - Spring 1998",
         "editor": [
             "Miwako Hisagi",
             "Jennifer Martin"
@@ -107,42 +138,42 @@ issues = [
         "article": [
             {
                 "title": "acknowledgements",
-                "file": "5_ack.pdf"
+                "file": "texts/issue_5/5_ack.pdf"
             },
             {
                 "author": "Miwako Hisagi:",
                 "title": "Mora versus Syllable: An Analysis of Native Speakers' Production from a Japanese Language Game",
-                "file": "5_Hisagi.pdf"
+                "file": "texts/issue_5/5_Hisagi.pdf"
             },
             {
                 "author": "Nikoleta Dineen and Daiva Miller:",
                 "title": "The Derivation of a Sonority Hierarchy from the Syllable Contact Law (SCL) and the Productivity of the SCL in American English",
-                "file": "5_Dineen.pdf"
+                "file": "texts/issue_5/5_Dineen.pdf"
             },
             {
                 "author": "Anthhony Nguyen and Carol Brouha:",
                 "title": "The Production of Word-Final Consonants in English by L1 Speakers of Vietnamese",
-                "file": "5_Nguyen.pdf"
+                "file": "texts/issue_5/5_Nguyen.pdf"
             },
             {
                 "author": "Jane Kirsch:",
                 "title": "Negative Contractions and Extraction Sites",
-                "file": "5_Kirsch.pdf"
+                "file": "texts/issue_5/5_Kirsch.pdf"
             },
             {
                 "author": "Cathleen O'Brien:",
                 "title": "The Jerry Springer Show Takes on the Cooperative Principle",
-                "file": "5_Obrien.pdf"
+                "file": "texts/issue_5/5_Obrien.pdf"
             },
             {
                 "author": "Marisa Schaer:",
                 "title": "Command of the Language",
-                "file": "5_Schaer.pdf"
+                "file": "texts/issue_5/5_Schaer.pdf"
             }
         ]
     },
     {
-        "id": "Volume 6, Fall 1999",
+        "id": "Volume 6 - Fall 1999",
         "editor": [
             "Miwako Hisagi",
             "Marietta Bradinova"
@@ -150,52 +181,52 @@ issues = [
         "article": [
             {
                 "title": "Acknowledgements",
-                "file": "6_ack.pdf"
+                "file": "texts/issue_6/6_ack.pdf"
             },
             {
                 "author": "Kathleen Baker:",
                 "title": "Seussisms and Violations to Universal Language Constraints",
-                "file": "6_Baker.pdf"
+                "file": "texts/issue_6/6_Baker.pdf"
             },
             {
                 "author": "Dawn Heverly:",
                 "title": "Phonological Aspects of Glossolalia: A response to Motley",
-                "file": "6_Heverly.pdf"
+                "file": "texts/issue_6/6_Heverly.pdf"
             },
             {
                 "author": "Sheryl Sherwin:",
                 "title": "The Sonority Sequencing Principle in Interlanguaage Phonology",
-                "file": "6_Sherwin.pdf"
+                "file": "texts/issue_6/6_Sherwin.pdf"
             },
             {
                 "author": "John Alphin:",
                 "title": "Universal Grammar and Language Pedagogy: Has SLA Research Linked Theory and Practice?",
-                "file": "6_Alphin.pdf"
+                "file": "texts/issue_6/6_Alphin.pdf"
             },
             {
                 "author": "Anthony Nguyen:",
                 "title": "The Systematic Reduction of English Syllable Final Consonants",
-                "file": "6_Nguyen.pdf"
+                "file": "texts/issue_6/6_Nguyen.pdf"
             },
             {
                 "author": "Marietta Bradinova:",
                 "title": "Subject Control and Nonsubject Control",
-                "file": "6_Bradinova.pdf"
+                "file": "texts/issue_6/6_Bradinova.pdf"
             },
             {
                 "author": "Terri Rhyne:",
                 "title": "It, There and First Conjuncts",
-                "file": "6_Rhyne.pdf"
+                "file": "texts/issue_6/6_Rhyne.pdf"
             },
             {
                 "author": "-Brian Rose:",
                 "title": "<i>-Ing<\/i>lish 101",
-                "file": "6_Rose.pdf"
+                "file": "texts/issue_6/6_Rose.pdf"
             }
         ]
     },
     {
-        "id": "Volume 7, Fall 2000",
+        "id": "Volume 7 - Fall 2000",
         "editor": [
             "Christina Villafa&ntilde;a",
             "Sonja Knecht-Hoshi"
@@ -203,52 +234,52 @@ issues = [
         "article": [
             {
                 "title": "Acknowledgements",
-                "file": "7_ack.pdf"
+                "file": "texts/issue_7/7_ack.pdf"
             },
             {
                 "author": "Maria S. Beldon:",
                 "title": "Order Preference for Reduplicated Words with Differing Vowels",
-                "file": "7_Beldon.pdf"
+                "file": "texts/issue_7/7_Beldon.pdf"
             },
             {
                 "author": "Peter T. Ruffner:",
                 "title": "Epenthesis and Fricatives in Final Consonant Clusters",
-                "file": "7_Ruffner.pdf"
+                "file": "texts/issue_7/7_Ruffner.pdf"
             },
             {
                 "author": "Christina Villafa&ntilde;a:",
                 "title": "Emergence of the Unmarked in Interlanguage Coda Production",
-                "file": "7_Villafana.pdf"
+                "file": "texts/issue_7/7_Villafana.pdf"
             },
             {
                 "author": "Kathleen Baker:",
                 "title": "Synonymy: Not the Sole Cause of Blocking",
-                "file": "7_Baker.pdf"
+                "file": "texts/issue_7/7_Baker.pdf"
             },
             {
                 "author": "Melissa R. Olt:",
                 "title": "Semantics in Support of the Quantifier Float",
-                "file": "7_Olt.pdf"
+                "file": "texts/issue_7/7_Olt.pdf"
             },
             {
                 "author": "Christina Villafa&ntilde;a:",
                 "title": "Subject Prominence in English Middles",
-                "file": "7_Villafana.pdf"
+                "file": "texts/issue_7/7_Villafana.pdf"
             },
             {
                 "author": "Szilvia Oszko:",
                 "title": "A Lexicalist Approach To Absolute Reflexives",
-                "file": "7_Oszko.pdf"
+                "file": "texts/issue_7/7_Oszko.pdf"
             },
             {
                 "author": "Abby Brown:",
                 "title": "\"What happened to my Yiddish?\"",
-                "file": "7_Brown.pdf"
+                "file": "texts/issue_7/7_Brown.pdf"
             }
         ]
     },
     {
-        "id": "Volume 8, Fall 2011",
+        "id": "Volume 8 - Fall 2011",
         "editor": [
             "Judith Hadley",
             "Tel Monks",
@@ -257,52 +288,53 @@ issues = [
         "article": [
             {
                 "title": "Acknowledgements",
-                "file": "8_ack.pdf"
+                "file": "texts/issue_8/8_ack.pdf"
             },
             {
                 "author": "Barbara Heigl:",
                 "title": "[s] Under the influence of alcohol.",
-                "file": "8_Heigl2.pdf"
+                "file": "texts/issue_8/8_Heigl2.pdf"
             },
             {
                 "author": "Thamir Hilanto:",
                 "title": "On Language Use: A Theoretical Treaty on the Workings of Language.",
-                "file": "8_Hilanto.pdf"
+                "file": "texts/issue_8/8_Hilanto.pdf"
             },
             {
                 "author": "Jodi Lamm",
                 "title": "Making Sense out of Nonsense Poetry.",
-                "file": "8_Lamm.pdf"
+                "file": "texts/issue_8/8_Lamm.pdf"
             },
             {
                 "author": "Shanna Phillips:",
                 "title": "Exceptionality of /s/: Evidence from Aphasia.",
-                "file": "8_Phillips.pdf"
+                "file": "texts/issue_8/8_Phillips.pdf"
             },
             {
                 "author": "Barbara Rachlin:",
                 "title": "Derrida's Questionable Logic.",
-                "file": "8_Rachlin2.pdf"
+                "file": "texts/issue_8/8_Rachlin2.pdf"
             },
             {
                 "author": "Barbara Rachlin:",
                 "title": "Looking for Searle's Black Swan",
-                "file": "8_Rachlin1.pdf"
+                "file": "texts/issue_8/8_Rachlin1.pdf"
             },
             {
                 "author": "Tanja Szabo:",
                 "title": "A Study of Monopthongization of /aj/ to /a:/ in Rural Eastern Kentucky and West Virginia Appalachian Regions.",
-                "file": "8_Szabo.pdf"
+                "file": "texts/issue_8/8_Szabo.pdf"
             },
             {
                 "author": "Benjamin F. Taylor:",
                 "title": "Why not \"Spop\"?: OCP and Prominent Position Effects on the English Lexicon.",
-                "file": "8_Taylor.pdf"
+                "file": "texts/issue_8/8_Taylor.pdf"
             }
         ]
     },
     {
-        "id": "Volume 9, Fall 2013",
+        "id": "Volume 9 - Fall 2013",
+        "full_issue": "texts/issue_9/WPILL_volume_9.pdf",
         "editor": [
             "Katie Shoaf",
             "Claudia Calissie",
@@ -311,43 +343,63 @@ issues = [
         "article": [
             {
                 "title": "Acknowledgements",
-                "file": "9_ack.pdf"
+                "file": "texts/issue_9/9_ack.pdf"
             },
             {
                 "author": "Chris Alderman:",
                 "title": "Glossomimesis.",
-                "file": "9_ChrisAlderman.pdf"
+                "file": "texts/issue_9/9_ChrisAlderman.pdf"
             },
             {
                 "author": "John Kinney:",
                 "title": "The L2 English production of [ð] in word-initial onset and intervocalic onset position: A pilot study.",
-                "file": "9_JohnKinney.pdf"
+                "file": "texts/issue_9/9_JohnKinney.pdf"
             },
             {
                 "author": "Justin Voigt:",
                 "title": "The Distinction between Noun-Phrase Premodifiers: Nouns are not Adjectives.",
-                "file": "9_JustinVoigt_NP Premodifiers.pdf"
+                "file": "texts/issue_9/9_JustinVoigt_NP Premodifiers.pdf"
             },
             {
                 "author": "Justin Voigt:",
                 "title": "Can the Syntactic Burden on Working Memory Account for Island Constraints?.",
-                "file": "9_JustinVoigt_Working Memory.pdf"
+                "file": "texts/issue_9/9_JustinVoigt_Working Memory.pdf"
             },
             {
                 "author": "Kelly Enochson:",
                 "title": "A binary features approach to person and number asymmetry.",
-                "file": "9_KellyEnochson.pdf"
+                "file": "texts/issue_9/9_KellyEnochson.pdf"
             },
             {
                 "author": "Michael Klein:",
                 "title": "Aspir(at)ing to Speak Like a Native: Tracking Voice Onset Time in the Acquisition of English Stops.",
-                "file": "9_MikeKlein.pdf"
+                "file": "texts/issue_9/9_MikeKlein.pdf"
             },
             {
                 "author": "R. Cameron Petty:",
                 "title": "Matrix Relationship Theory.",
-                "file": "9_R. Cameron Petty.pdf"
+                "file": "texts/issue_9/9_R. Cameron Petty.pdf"
             }
+        ]
+    },
+    {
+        "id": "Volume 10 - Spring 2022",
+        "full_issue": "texts/issue_10/WPILL_volume_10.pdf",
+        "editor": [
+            "Benjamin Hunt",
+            "Jonathan M Marine"
+        ],
+        "article": [
+            {
+                "title": "Acknowledgements",
+                "file": "texts/issue_10/10_ack.pdf"
+            },
+            {
+                "author": "Your Mom:",
+                "title": "A test paper: Testing the tests.",
+                "file": "texts/issue_10/10_test.pdf"
+            },
+            
         ]
     }
 ];
